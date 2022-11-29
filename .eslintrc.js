76dcf1abf422b93af6cbc372c0e30b1277de47e5
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-11-29 14:38:39 -0600

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'consistent-return': 'off',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'prettier/prettier': 'warn',
+    'react/jsx-no-bind': 'off',
+  },
+};
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+import './index.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(<App />);
